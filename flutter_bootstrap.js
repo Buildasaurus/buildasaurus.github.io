e8caacca6a811b7ba4a743caf7f9c92af3c98c60
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-10-25 15:36:45 +0200

    Update web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "506544928"
+    serviceWorkerVersion: "3827415484"
   }
 });
